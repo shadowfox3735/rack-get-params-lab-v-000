66e86db8fd517e35ef852473f48e93a82d58ad3e
shadowfox3735 <normanpan180@gmail.com>
2019-05-12 21:30:44 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -23,7 +23,7 @@ class Application
         end
       end
     elsif req.path.match(/add/)
-      req.params["item"]
+      item = req.params["item"]
       if @@items.include?(item)
         @@cart << item
         resp.write "added #{item}"
